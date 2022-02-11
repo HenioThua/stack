b134a3486d9185b5d581b6ab2f745926976a6c67
Hênio Thuãn <heniothua13@gmail.com>
2022-02-11 10:43:47 -0300

    add stack

---
+++ stack.py
@@ -0,0 +1,13 @@
+class stack():
+    def __init__(self):
+        self.l = []
+
+    def isEmpty(self):
+        return len(self.l) == 0 
+
+    def push(self, value):
+        self.l.append(value)
+        
+    def pop(self):
+        return self.l.pop()
+        
